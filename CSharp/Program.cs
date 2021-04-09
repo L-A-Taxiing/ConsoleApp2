d6d92b0846c778170e9354e2393b6a34a577e040
Peng Jiang <347232215@qq.com>
2021-04-09 10:18:31 +0800

    C#作业；个人页面，变量类型。

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -5,8 +5,26 @@ namespace CSharp
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            string icon = "头像";
+            bool gender = true;
+            string starsign = "双鱼座";
+            int birthday = 38;
+            String keyword = "编程开发语言";
+            string selfintroduction = "大噶猴，我系渣渣辉";
+
+
+            Console.WriteLine(icon);
+            Console.WriteLine(starsign);
+            Console.WriteLine(gender);
+            Console.WriteLine(birthday);
+            Console.WriteLine(keyword);
+            Console.WriteLine(selfintroduction);
+
+
+
+
         }
     }
 }
