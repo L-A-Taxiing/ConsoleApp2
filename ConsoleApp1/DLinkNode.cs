4f913098e78883eba4fa458b02fb51a701be15fb
Peng Jiang <347232215@qq.com>
2021-04-28 19:47:03 +0800

    添加新类DLinkNodeTests

--- ConsoleApp1/DLinkNode.cs
+++ ConsoleApp1/DLinkNode.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class DLinkNode
+    public class DLinkNode
     {
+        public DLinkNode Previous{get;set;}
+        public DLinkNode Next { get; set; }
+
+        public void Add(DLinkNode node)
+        {
+        
+        }
+
+        public void Delete() { }
+        public void Swap() { }
+
+             
+
     }
 }
