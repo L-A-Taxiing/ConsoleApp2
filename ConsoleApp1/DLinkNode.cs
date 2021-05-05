1a9013f09d9b797c90a9af35c74849edd9845dee
Peng Jiang <347232215@qq.com>
2021-05-05 11:10:12 +0800

    C#-面向对象-双向链表 ○|￣|_

--- ConsoleApp1/DLinkNode.cs
+++ ConsoleApp1/DLinkNode.cs
@@ -8,35 +8,88 @@ namespace ConsoleApp1
 {
     public class DLinkNode
     {
-        public DLinkNode Previous{get;set;}
+        public DLinkNode Previous { get; set; }
         public DLinkNode Next { get; set; }
 
         public void AddAfter(DLinkNode node)
         {
-            if (this.Next==null)
+            if (this.Next == null)
             {
                 node.Previous = this;
                 this.Next = node;
             }
             else
             {
-
                 node.Next = this.Next;
-                node.Previous = this;
                 this.Next.Previous = node;
+                node.Previous = this;
                 this.Next = node;
-
+                
             }
 
         }
-        public void Addbefore(DLinkNode node) 
+        public void AddBefore(DLinkNode node)
+        {
+            // 2  [1]  3
+            if (this.Previous==null)
+            {
+                node.Next = this;
+                this.Previous = node;
+            }
+            else
+            {
+                node.Previous = this.Previous;
+                this.Previous.Next = node;
+                node.Next = this;
+                this.Previous = node;
+            }
+        }
+        public void Delete() 
+        {
+            if (this.Previous==null&&this.Next!=null)//   [1] 2 3 4 5
+            {
+                DLinkNode node = this.Next;
+                this.Next = null;
+                node.Previous = null;
+
+            }
+            else if (this.Next==null&&this.Previous!=null)//2 3 4 [5]
+            {
+                DLinkNode node = this.Previous;
+                this.Previous = null;
+                node.Next = null;
+            }
+            else if (this.Previous!=null&&this.Next!=null)/*2 [3] 4*/
+            {
+                this.Previous = this.Previous;
+                this.Next = this.Next;
+            }
+            else // [2]
+            {
+                this.Previous = null;
+                this.Next = null;
+            }
+        }
+        public void Swap(DLinkNode node) 
+        {
+            //if (this.Previous==null&&node.Next!=null)
+            //{
+            //    //node.Previous.Next = this;
+            //    //this.Previous = node.Previous;
+            //    //this.Next = node.Next;
+            //    //node.Next.Previous = this;
+
+            //    //node = this.Previous.Previous;
+            //    //node.Previous = null;
+            //    //node.Next = this;
+
+            //}
+
+        }
+        public void FindBy() 
         {
         
         }
-        public void Delete() { }
-        public void Swap() { }
-
-             
 
     }
 }
--- TestProject1/DLinkeNodeTests.cs
+++ TestProject1/DLinkeNodeTests.cs
@@ -8,59 +8,204 @@ namespace TestProject1
 {
     public class DLinkeNodeTests
     {
-        private DLinkNode node1, node2, node3, node4;
+        private DLinkNode node1, node2, node3, node4,node5;
         //1,2,4,3
         [SetUp]//每次测试自动调用，就不需要调用了
         public void SetUp() 
         {
-            /*DLinkNode*/
             node1 = new DLinkNode();
-            /*DLinkNode*/
             node2 = new DLinkNode();
-            /*DLinkNode*/
             node3 = new DLinkNode();
-            /*DLinkNode*/
             node4 = new DLinkNode();
-
-            node1.Next = node2;
-            node2.Previous = node1;
-            node2.Next = node4;
-            node4.Previous = node2;
-            node4.Next = node3;
-            node3.Previous = node4;
-            //node1.AddAfter（node2）;
-            //会报异常，运行时需要测试；
-
-
+            node5 = new DLinkNode();
 
         }
 
-
         [Test]
-        public void AddTest() 
+        public void AddAfterTest1() 
         {
             ////1  [2]
             //Assert.IsNull(node1.Previous);
             //Assert.AreEqual(node2, node1.Next);
 
-
             //Assert.AreEqual(node1, node2.Previous);
             //Assert.IsNull(node2.Next);
-
-
 
             ////1 2 [3]
             //Assert.IsNull(node3.Next);
             //Assert.AreEqual(node2, node3.Previous);
             //Assert.AreEqual(node3, node2.Next);
 
-            //1 2 [4] 3
 
+            //1 2 [4] 3
+            node1.AddAfter(node2);
+            node2.AddAfter(node4);
+            node4.AddAfter(node3);
             Assert.AreEqual(node4, node2.Next);
             Assert.AreEqual(node2, node4.Previous);
             Assert.AreEqual(node3, node4.Next);
             Assert.AreEqual(node4, node3.Previous);
             Assert.IsNull(node3.Next);
+        }
+        public void AddAfterTest2()
+        {
+            //1.node3.InsertAfter(node1); 场景
+            //[1] 3
+            node1.AddAfter(node3);
+            Assert.IsNull(node3.Next);
+            Assert.IsNull(node1.Previous);
+            Assert.AreEqual(node1, node3.Previous);
+            Assert.AreEqual(node3, node1.Next);
+
+            //1 3 [4]
+            node3.AddAfter(node4);
+            Assert.AreEqual(node3, node4.Previous);
+            Assert.AreEqual(node4, node3.Next);
+            Assert.IsNull(node4.Next);
+
+            //1 3 [2] 4
+            node3.AddAfter(node2);
+            Assert.AreEqual(node3, node2.Previous);
+            Assert.AreEqual(node2, node3.Next);
+            Assert.AreEqual(node2, node4.Previous);
+            Assert.AreEqual(node4, node2.Next);
+        }
+        [Test]
+        public void AddBeforeTest() 
+        {
+            //2.InerstBefore()：在某个节点前插入
+            //[1] 2
+            node2.AddBefore(node1);
+            Assert.AreEqual(node1,node2.Previous);
+            Assert.AreEqual(node2,node1.Next);
+            Assert.IsNull(node1.Previous);
+            Assert.IsNull(node2.Next);
+
+            //[3] 1 2
+            node1.AddBefore(node3);
+            Assert.AreEqual(node1,node3.Next);
+            Assert.AreEqual(node3,node1.Previous);
+            Assert.IsNull(node3.Previous);
+
+            //3 [4] 1 2
+            node1.AddBefore(node4);
+            Assert.AreEqual(node4, node1.Previous);
+            Assert.AreEqual(node3, node4.Previous);
+            Assert.AreEqual(node4, node3.Next);
+            Assert.AreEqual(node1, node4.Next);
+
+
+        }
+
+        [Test]
+        public void DeleteTest() 
+        {
+            node1.AddAfter(node2);
+            node2.AddAfter(node3);
+            node3.AddAfter(node4);
+            node4.AddAfter(node5);
+            // [1] 2 3 4 5
+            node1.Delete();
+            Assert.IsNull(node2.Previous);
+            Assert.IsNull(node1.Next);
+            Assert.IsNull(node1.Previous);
+            //2 [3] 4 5
+            node3.Delete();
+            Assert.IsNull(node2.Previous);
+            Assert.AreEqual(node4, node2.Next);
+            Assert.AreEqual(node2,node4.Previous);
+            Assert.AreEqual(node5,node4.Next);
+            //2 [4] 5
+            node4.Delete();
+            Assert.IsNull(node2.Previous);
+            Assert.AreEqual(node5, node2.Next);
+            Assert.AreEqual(node2, node5.Previous);
+            Assert.IsNull(node5.Next);
+            //2 [5]
+            node5.Delete();
+            Assert.IsNull(node2.Previous);
+            Assert.IsNull(node2.Next);
+            Assert.AreEqual(node2, node5.Previous);
+            Assert.IsNull(node5.Next);
+            //[2]
+            node2.Delete();
+            Assert.IsNull(node2.Previous);
+            Assert.IsNull(node2.Next);
+
+        }
+
+
+        [Test]
+        public void SwapTest1() 
+        {
+            node1.AddAfter(node2);
+            node2.AddAfter(node3);
+            node3.AddAfter(node4);
+            node4.AddAfter(node5);
+            //4.[选] Swap()：交互某两个节点
+            //[1] 2 [3] 4 5  =>3 2 1 4 5      首和中间交换
+            node1.Swap(node3);
+            Assert.AreEqual(node2,node1.Previous);
+            Assert.AreEqual(node1,node2.Next);
+            Assert.AreEqual(node4,node1.Next);
+            Assert.AreEqual(node1,node4.Previous);
+            Assert.AreEqual(null,node3.Previous);
+            Assert.AreEqual(node2,node3.Next);
+            Assert.AreEqual(node3, node2.Previous);
+
+
+            //[3]  2  1  4  [5]  =>5 2 1 4 3      首尾交换
+            node3.Swap(node5);
+            Assert.AreEqual(node4,node3.Previous);
+            Assert.AreEqual(node3,node4.Next);
+            Assert.AreEqual(null,node3.Next);
+            Assert.AreEqual(null,node5.Previous);
+            Assert.AreEqual(node2,node5.Next);
+            Assert.AreEqual(node5,node2.Previous);
+
+
+            //5  [2]  1  4  [3] =>5 3 1 4 2        中间和尾交换
+            node2.Swap(node3);
+            Assert.AreEqual(node4,node2.Previous);
+            Assert.AreEqual(node2,node4.Next);
+            Assert.AreEqual(null,node2.Next);
+            Assert.AreEqual(node5,node3.Previous);
+            Assert.AreEqual(node3,node5.Next);
+            Assert.AreEqual(node1,node3.Next);
+            Assert.AreEqual(node3,node1.Previous);
+
+            //5  [3]   1  [4]   2 =>5 4 1 3 2       中间和中间交换
+            node3.Swap(node4);
+            Assert.AreEqual(node1,node3.Previous);
+            Assert.AreEqual(node3,node1.Next);
+            Assert.AreEqual(node2,node3.Next);
+            Assert.AreEqual(node3,node2.Previous);
+            Assert.AreEqual(node5,node4.Previous);
+            Assert.AreEqual(node4,node5.Next);
+            Assert.AreEqual(node1,node4.Next);
+            Assert.AreEqual(node4,node1.Previous);
+
+
+        }
+        [Test]
+        public void SwapTest2() 
+        {
+            //[1] [2]=>2 1
+            node1.AddAfter(node2);
+            node1.Swap(node2);
+            Assert.AreEqual(node2,node1.Previous);
+            Assert.AreEqual(node1,node2.Next);
+            Assert.AreEqual(null,node1.Next);
+            Assert.AreEqual(null,node2.Previous);
+            Assert.AreEqual(node1,node2.Next);
+            Assert.AreEqual(node2, node1.Previous);
+        }
+
+        [Test]
+        public void FindByTest() 
+        {
+            //5.[选] FindBy()：根据节点值查找到某个节点
+
 
 
 
