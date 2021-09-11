bbb357b60ec3c9c83b51eb9351f8fb4adb522694
Peng Jiang <347232215@qq.com>
2021-09-11 16:21:10 +0800

    新建C#2类熟悉反射

---
+++ CSharp2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace CSharp2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student wxy = new Student();
+            Console.WriteLine(wxy.GetType()
+                .GetField("Name",
+                BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue(wxy)); 
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ CSharp2.0/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp2._0
+{
+    class Student
+    {
+        private String Name="王新玉";
+        
+
+
+
+    }
+}
